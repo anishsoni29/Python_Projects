9192c5454e95a6a765a1a064605c62e8379058ce
Anish Soni <anish.21cse189@rtu.ac.in>
2023-08-18 23:39:13 +0530

    Changes

--- Indian States Quiz/main.py
+++ Indian States Quiz/main.py
@@ -17,7 +17,7 @@ while len(guessed_states) < 50:
 
     if answer_state == "Exit":
         missing_states = [state for state in all_states if state not in guessed_states]
-        #Used list comprehension here to reduce the loc.
+        #Implemented list comprehension.
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         print(missing_states)
