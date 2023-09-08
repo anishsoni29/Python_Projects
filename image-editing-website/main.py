182910dcdd6f85c8d10b16cbb23183bf166e7ecc
Anish Soni <anish.21cse189@rtu.ac.in>
2023-09-08 22:23:11 +0530

    fault in the code. Starting with the basic modules again.

--- image-editing-website/main.py
+++ image-editing-website/main.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+import cv2
+import os
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'webp', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def processImage(filename, operation):
+    print(f"the operation is {operation} and filename is {filename}")
+    img = cv2.imread(f'uploads/{filename}')
+    match operation:
+        case 'cgray':
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"static/{filename}", imgProcessed)
+            return filename
+    pass
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def home():
@@ -9,6 +31,29 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route("/edit", methods = ["GET", "POST"])
+def edit():
+    if request.method == 'POST':
+        operation = request.form.get('operation')
+         # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return 'error'
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return 'no file selected'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            processImage(filename, operation)
+            flash("Your image has been processed and is available <a href = '/static/{filename}'> here </a>")
+            return render_template('index.html')
+    
+    return render_template('index.html')
 
 #make a different route for each page
 #this was the error while creating that project
