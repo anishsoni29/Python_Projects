beb1804a453c5053d78aa5d3f2e012a7d2f15733
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-26 00:16:49 +0530

    finished off with the frontend code

--- CHAT-WITH-WEBSITE/src/app.py
+++ CHAT-WITH-WEBSITE/src/app.py
@@ -7,3 +7,14 @@ st.title("Chat with Website")
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
+
+st.chat_input("Type your message here...")
+
+with st.chat_message("AI"):
+    st.write("Hello! How can I help you today?")
+
+with st.chat_message("Human"):
+    st.write("I want to know about Langchain.")
+
+with st.chat_message("AI"):
+    st.write("No")
