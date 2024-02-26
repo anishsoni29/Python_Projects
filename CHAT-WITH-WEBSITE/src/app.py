3b2e4229ab357b273d2d924c5b3171275ac75839
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-26 23:48:28 +0530

    added some streamlit code

--- CHAT-WITH-WEBSITE/src/app.py
+++ CHAT-WITH-WEBSITE/src/app.py
@@ -1,4 +1,6 @@
+
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 
 def get_response(user_query):
     return "I don't know!"
@@ -6,6 +8,11 @@ def get_response(user_query):
 #app config
 st.set_page_config(page_title = "Chat with Website", page_icon = "🤖")
 st.title("Chat with Website")
+
+#schemas - building bots of langchain models.
+chat_history = [
+    AIMessage(content="Hello! I am a chatbot. How can I help you?"),
+]
 
 #sidebar
 with st.sidebar:
@@ -16,8 +23,10 @@ with st.sidebar:
 user_query = st.chat_input("Type your message here...")
 if user_query is not None and user_query != "":
     response = get_response(user_query)
-    with st.chat_message("Human"):
-        st.write(user_query)
+    chat_history.append(HumanMessage(content=user_query))
+    chat_history.append(AIMessage(content=response))
 
-    with st.chat_message("AI"):
-        st.write(response)
+#chat history
+
+with st.sidebar:
+    st.write(chat_history)
