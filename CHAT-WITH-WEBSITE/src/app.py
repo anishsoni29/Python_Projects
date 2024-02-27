830ae7b7711575cba0eeae0f9ec4682aee8db5ab
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-28 01:31:25 +0530

    updated the final code with the new api key

--- CHAT-WITH-WEBSITE/src/app.py
+++ CHAT-WITH-WEBSITE/src/app.py
@@ -1,3 +1,4 @@
+# pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
 
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
@@ -7,66 +8,97 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
 
 load_dotenv()
 
-
-def get_response(user_query):
-    return "I don't know!"
-
-def get_vectorstore_from_url(user_input):
-    #get the text in document form
-    loader = WebBaseLoader(user_input)
-    documents = loader.load()
+def get_vectorstore_from_url(url):
+    # get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
     
-    #split documents into chunks
+    # split the document into chunks
     text_splitter = RecursiveCharacterTextSplitter()
-    documents = text_splitter.split_documents(documents)
+    document_chunks = text_splitter.split_documents(document)
+    
+    # create a vectorstore from the chunks
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
 
-    #create a vectorstore from the chunks
-    vector_store = Chroma.from_documents(documents_chunks, OpenAIEmbeddings())
     return vector_store
 
-def get_context_retreiver_chain(vector_store):
+def get_context_retriever_chain(vector_store):
     llm = ChatOpenAI()
+    
     retriever = vector_store.as_retriever()
+    
     prompt = ChatPromptTemplate.from_messages([
-        MessagesPlaceholder(variable_name="chat_history"),
-        ("user", "{input}"),
-        ("user", "Given the context above, what is the answer?")
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
     ])
+    
     retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
     return retriever_chain
+    
+def get_conversational_rag_chain(retriever_chain): 
+    
+    llm = ChatOpenAI()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
 
-#app config
-st.set_page_config(page_title = "Chat with Website", page_icon = "🤖")
-st.title("Chat with Website")
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_query
+    })
+    
+    return response['answer']
 
-#schemas - building bots of langchain models.
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [
-        AIMessage(content="Hello! I am a bot. How can I help you?")
-    ]
+# app config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites")
 
-#sidebar
+# sidebar
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
 
 if website_url is None or website_url == "":
-    st.warning("Please enter a website URL")
-else:
-    documents_chunks = get_vectorstore_from_url(website_url)
+    st.info("Please enter a website URL")
 
-    #user input
+else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+
+    # user input
     user_query = st.chat_input("Type your message here...")
     if user_query is not None and user_query != "":
         response = get_response(user_query)
         st.session_state.chat_history.append(HumanMessage(content=user_query))
         st.session_state.chat_history.append(AIMessage(content=response))
+        
+       
 
-    #conversation 
+    # conversation
     for message in st.session_state.chat_history:
         if isinstance(message, AIMessage):
             with st.chat_message("AI"):
@@ -74,14 +106,3 @@ else:
         elif isinstance(message, HumanMessage):
             with st.chat_message("Human"):
                 st.write(message.content)
-
-
-# 1. chunk of text(document)
-# 2. embeddings --> 010101
-# 3. Vector store --> database
-# 4. ranked results
-# 5. LLM
-# 6. Answer
-# 7. Human 
-# 8. question embedding
-# 9. semantic search
