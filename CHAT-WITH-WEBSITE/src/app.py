81259fbf04f0afd64b9b8a075bde2a1e37bcc423
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-27 21:42:56 +0530

    frontend done till rag

--- CHAT-WITH-WEBSITE/src/app.py
+++ CHAT-WITH-WEBSITE/src/app.py
@@ -20,14 +20,21 @@ with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
 
-#user input
-user_query = st.chat_input("Type your message here...")
-if user_query is not None and user_query != "":
-    response = get_response(user_query)
-    st.session_state.chat_history.append(HumanMessage(content=user_query))
-    st.session_state.chat_history.append(AIMessage(content=response))
+if website_url is None or website_url == "":
+    st.warning("Please enter a website URL")
+else:
+    #user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
 
-#chat history
-
-with st.sidebar:
-    st.write(st.session_state.chat_history)
+    #conversation 
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
